8cdb43229dc42c8b4302f42add5c06837c455d2e
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-05-14 21:27:36 +0100

    add common module

---
+++ src/common.rs
@@ -0,0 +1,50 @@
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    path::PathBuf,
+    sync::{Arc, RwLock},
+};
+
+/// TODO: maybe change from zkVMType to zkVMVendor
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ZkVMType {
+    Risc0,
+    SP1,
+}
+
+impl std::str::FromStr for ZkVMType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "risc0" => Ok(ZkVMType::Risc0),
+            "sp1" => Ok(ZkVMType::SP1),
+            _ => Err(format!(
+                "Unsupported zkVM type: {}. Supported types are: risc0, sp1",
+                s
+            )),
+        }
+    }
+}
+
+impl std::fmt::Display for ZkVMType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ZkVMType::Risc0 => write!(f, "risc0"),
+            ZkVMType::SP1 => write!(f, "sp1"),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Program {
+    Risc0(String),
+    SP1(String),
+}
+
+#[derive(Clone)]
+pub struct AppState {
+    pub programs_dir: PathBuf,
+    pub programs: Arc<RwLock<HashMap<String, Program>>>,
+}
