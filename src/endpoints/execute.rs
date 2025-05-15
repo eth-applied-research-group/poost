f5bf66d5a05a7429c91e7395f3ff389df96e1228
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-05-15 15:21:56 +0100

    use tokio::sync::RwLock and add basic invalid test

--- src/endpoints/execute.rs
+++ src/endpoints/execute.rs
@@ -27,7 +27,7 @@ pub async fn execute_program(
     State(state): State<AppState>,
     Json(_req): Json<ExecuteRequest>,
 ) -> Result<Json<ExecuteResponse>, (StatusCode, String)> {
-    if let Some(_program) = state.programs.read().unwrap().get(&program_id) {
+    if let Some(_program) = state.programs.read().await.get(&program_id) {
         // TODO: stub program, real program will check the zkvm type and then
         // TODO: choose the zkvm to execute the program
         Ok(Json(ExecuteResponse {
@@ -37,5 +37,44 @@ pub async fn execute_program(
         }))
     } else {
         Err((StatusCode::NOT_FOUND, "Program not found".to_string()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        collections::HashMap,
+        env::temp_dir,
+        sync::Arc,
+    };
+    use tokio::sync::RwLock;
+
+    use axum::{
+        Json,
+        extract::{Path, State},
+    };
+
+    use crate::{
+        common::AppState,
+        endpoints::{execute::ExecuteRequest, execute_program},
+    };
+
+    #[tokio::test]
+    async fn test_execute_program() {
+        let state = AppState {
+            programs_dir: temp_dir(),
+            programs: Arc::new(RwLock::new(HashMap::new())),
+        };
+
+        let req = ExecuteRequest {
+            input: "input_to_program".to_string(),
+        };
+
+        let result =
+            execute_program(Path("non_existent".to_string()), State(state), Json(req)).await;
+
+        assert!(result.is_err());
+        let (status, _) = result.err().unwrap();
+        assert_eq!(status, axum::http::StatusCode::NOT_FOUND);
     }
 }
