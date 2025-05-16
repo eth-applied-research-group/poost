585555fec0da994e9197d7921b98e5693a9b522d
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-05-16 11:34:11 +0100

    remove register endpoint

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,11 +1,9 @@
 pub mod execute;
 pub mod info;
 pub mod prove;
-pub mod register;
 pub mod verify;
 
 pub use execute::execute_program;
 pub use info::get_server_info;
 pub use prove::prove_program;
-pub use register::register_program;
 pub use verify::verify_proof;
